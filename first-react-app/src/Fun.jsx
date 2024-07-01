ec8c07e0fef4ed2f19c7968ad85ba9360bf964e9
Shantanu Kalidas Deshmane <144879318+Shantanu-Deshmane@users.noreply.github.com>
2024-07-01 21:04:17 +0530

    Update Fun.jsx
    Add h1 tag

--- first-react-app/src/Fun.jsx
+++ first-react-app/src/Fun.jsx
@@ -3,6 +3,7 @@ function Fun() {
     return (
 
         <>
+        <h1>Its React + Vite</h1>
         <h2>Let's do some Fun with react</h2>
         <p>React is full of adventures...!</p>
         </>
