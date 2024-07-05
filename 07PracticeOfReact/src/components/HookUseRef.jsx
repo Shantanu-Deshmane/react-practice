cd95bc61fe361aa3c38868328191fc5c7879f94a
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-07-05 21:55:58 +0530

    display input value on UI using useState and useRef Hooks

--- 07PracticeOfReact/src/components/HookUseRef.jsx
+++ 07PracticeOfReact/src/components/HookUseRef.jsx
@@ -1,14 +1,19 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 
 // useRef is a react hook using that we can take a reference from any element
 function HookUseRef() {
     // Accessing the value of input using the useRef Hook
     const inpRef = useRef(null)
+
+    const [data, setData] = useState([])
   return (
     <>
     <input ref={inpRef} type="text"  />
-    <button onClick={()=>{console.log(inpRef.current.value)}}>Submit</button>
+    <button onClick={()=>{setData([...data, inpRef.current.value])}}>Add Data</button>
+    
+    {data.map((item, index)=>{return <p key={index}>{item}</p>})}
+    
     </>
   )
 }
