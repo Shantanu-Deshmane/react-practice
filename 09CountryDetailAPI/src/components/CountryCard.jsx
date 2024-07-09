75382fd7cc752a60e46164a1115a3cbb534588da
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-07-09 23:11:02 +0530

    Add Link component to restrict the loading of page

--- 09CountryDetailAPI/src/components/CountryCard.jsx
+++ 09CountryDetailAPI/src/components/CountryCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import  {Link } from 'react-router-dom'
 
 function CountryCard({name, population, flag, region, capital}) {
     
   return (
     <>
         <div className='w-[250px] mx-4 mt-10 text-sm bg-white rounded-lg overflow-hidden shadow-sm'>
-            <a href={`/country?name=${name}`} className='inline-block'>
+          {/* use Link component to chamge page without loading the page  or to restrict the loading of page*/}
+            <Link to={`/country?name=${name}`} className='inline-block'>
             <img src={flag} alt= {name + ' flag'}  />
             <div className='px-4 py-4'>
                 <h3 className='text-lg font-bold mb-2 '>{name}</h3>
@@ -15,7 +17,7 @@ function CountryCard({name, population, flag, region, capital}) {
                 <p className='py-2'><b className='font-semibold'>Region :</b> {region}</p>
                 <p className='py-2'><b className='font-semibold'>Capital :</b> {capital} </p>
             </div>
-            </a>
+            </Link>
         </div>
     </>
   )
