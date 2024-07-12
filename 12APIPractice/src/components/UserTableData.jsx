01e3a6353bc01f75da6d087f03804cd29f962d3c
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-07-12 21:24:50 +0530

    inital commit

--- 12APIPractice/src/components/UserTableData.jsx
+++ 12APIPractice/src/components/UserTableData.jsx
@@ -16,10 +16,11 @@ function UserTableData() {
         <table border={1}>
             <thead>
                 <tr>
+                    <th>Id</th>
                     <th>Name</th>
                     <th>Email</th>
                     <th>Address</th>
-                    <th>phone</th>
+                    <th>Postal Code</th>
                     <th>gender</th>
                     <th>Department</th>
                     <th>Designation</th>
@@ -28,11 +29,12 @@ function UserTableData() {
             <tbody>
             {userData.map((user) => (
                 <tr key={user.id}>
+                <td>{user.id}</td>
                 <td>{user.firstName} {user.lastName}</td>
                 <td>{user.email}</td>
                 <td>{user.address.address} {user.address.city}</td>
-                <td>{user.address.address} {user.address.city}</td>
-                <td>{user.address.address} {user.address.city}</td>
+                <td>{user.address.postalCode}</td>
+                <td>{user.gender}</td>
                 <td>{user.company.department}</td>
                 <td>{user.company.title}</td>
                 </tr>
