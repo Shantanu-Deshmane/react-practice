719f87b43c9f78c2a08d21f1a8a5f1784087a79b
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-07-12 22:27:57 +0530

    add css

--- 13productAPI/src/components/ProductCard.jsx
+++ 13productAPI/src/components/ProductCard.jsx
@@ -16,15 +16,15 @@ function ProductCard() {
         {productData.map((item)=>{
           return(
             <>
-              <div className='w-[350px] mx-4 mt-10 text-sm bg-white rounded-lg overflow-hidden shadow-lg' >
-              <img className='w-full h-[400px] ' src={item.image} alt="/"  />
+              <div className='w-[550px] mx-4 mt-10 text-sm bg-white rounded-lg overflow-hidden shadow-lg flex'  >
+              <img className='w-[250px] h-[250px] p-4' src={item.image} alt="/"  />
 
             <div className='px-4 py-4 text-center '>
                 <h3 className='text-lg font-bold mb-2 '>{item.title}</h3>
-                <p className='py-2 text-lg'><b>price : </b>{`${item.price}$`}</p>
+                <p className='py-2 text-2xl text-blue-400  font-bold'>{`$${item.price}`}</p>
                 <p className='py-2'><b>category : </b>{item.category}</p>
 
-              <div className='flex justify-around items-end'>
+              <div className='flex justify-center gap-2 items-end'>
                 <button className='bg-blue-600 px-8 py-2 text-white '>Buy</button>
                 <button className='bg-transparent px-8 py-2 border text-black ' >Add to cart</button>
               </div>
