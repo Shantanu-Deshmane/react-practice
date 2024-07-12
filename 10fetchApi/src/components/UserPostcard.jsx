d157b38b18c4065be63caa5e6c11fae24c0e4d74
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-07-12 21:34:34 +0530

    clean unnecessary code

--- 10fetchApi/src/components/UserPostcard.jsx
+++ 10fetchApi/src/components/UserPostcard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function UserPostcard(props) {
+function UserPostcard({data}) {
   return (
     <>
     <div className='main'>
-        <img src={props.data.picture.large} alt="" />
-        <h3> <span>Name: </span> {props.data.name.first}</h3>
-        <p> <span>Contact: </span> {props.data.phone}</p>
-        <p> <span>Address: </span>{props.data.location.city}, {props.data.location.country}</p>
+        <img src={data.picture.large} alt="" />
+        <h3> <span>Name: </span> {data.name.first}</h3>
+        <p> <span>Contact: </span> {data.phone}</p>
+        <p> <span>Address: </span>{data.location.city}, {data.location.country}</p>
     </div>
     </>
   )
