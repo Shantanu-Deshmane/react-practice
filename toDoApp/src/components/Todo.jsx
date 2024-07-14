feb4c2b0553a346a516e75da34b4a0b341cbe997
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-07-14 21:40:16 +0530

    done with basic todo app

--- toDoApp/src/components/Todo.jsx
+++ toDoApp/src/components/Todo.jsx
@@ -24,7 +24,7 @@ function Todo() {
       <div className='w-[350px] bg-white px-6 py-8 rounded shadow-lg'>
         <h3 className='font-bold text-2xl mb-8'>ToDo App</h3>
 
-        <div className='w-full flex justify-around rounded-lg        overflow-hidden bg-slate-200'>
+        <div className='w-full flex justify-around rounded-lg        overflow-hidden bg-slate-200'>  
 
           <input 
           ref={inpRef}  
