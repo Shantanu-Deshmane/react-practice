2f647256efe844f999119b1904b8894f9f9beaee
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-07-15 21:57:26 +0530

    learn old way of creating route

--- 16react-router-DOM/src/components/NavBar.jsx
+++ 16react-router-DOM/src/components/NavBar.jsx
@@ -5,12 +5,13 @@ function NavBar() {
   return (
     <div className='navBar'>
         <ul>
-            <li>
-                {/* Instead of "<a>" tag here react uses "Link" from react-router-dom with 'to attribute to navigate the pages' */}
+            
+        {/* Instead of "<a>" tag here react uses "Link" from react-router-dom with 'to attribute to navigate the pages' */}
+        
         <Link to={'/'}>Home</Link>
         <Link to={'/contact'}>Contact</Link>
         <Link to={'/about'}>About</Link>
-            </li>
+            
         </ul>
     </div>
   )
