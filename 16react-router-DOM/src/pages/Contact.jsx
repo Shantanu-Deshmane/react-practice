4d9bd1095129e0c024900e8ca40499b11e8f1e3a
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-07-16 12:55:22 +0530

    Use outlet to show the address and contact component below the Button

--- 16react-router-DOM/src/pages/Contact.jsx
+++ 16react-router-DOM/src/pages/Contact.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
 function Contact() {
   return (
     <div>
         <h2 className='contact'>Contact</h2>
-        <button> <NavLink to={'contact-us'} e>Contact</NavLink> </button>
-        <button><NavLink to={'address'}>Drop Letter</NavLink></button>
+        <button> <NavLink to={'contact-us'}>Contact</NavLink> </button>
+        <button> <NavLink to={'address'}>Drop Letter</NavLink> </button>
+        <Outlet/>
     </div>
   )
 }
