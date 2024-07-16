41a691b3bc11143de55adc69da773a5e00c8d927
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-07-16 16:26:32 +0530

    Learn dynamic routing

--- 16react-router-DOM/src/pages/Products.jsx
+++ 16react-router-DOM/src/pages/Products.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import { Link, Outlet } from 'react-router-dom';
 
 function Products() {
+  let data = ['123', '456', '145','456', '4545']
   return (
     <div>
-        <Link to={'123'} style={{marginRight:"15px", color:"Violet"}}>Product: 123</Link>
-        <Link to={'456'} style={{marginRight:"15px", color:"Violet"}}>Product: 456</Link>
-        <Link to={'789'} style={{marginRight:"15px", color:"Violet"}}>Product: 789</Link>
 
-        <Outlet/>
+      {data.map((id) => (
+        
+          <Link to={id} style={{ marginRight: "15px", color: "Violet" }}>Product: {id}</Link>
+      ))
+      }
+
+      {/* <Link to={'456'} style={{marginRight:"15px", color:"Violet"}}>Product: 456</Link>
+        <Link to={'789'} style={{marginRight:"15px", color:"Violet"}}>Product: 789</Link> */}
+
+      <Outlet />
     </div>
   )
 }
