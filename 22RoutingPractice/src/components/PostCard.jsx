5ebf103170a56af08bb404458b7a6e3dc86f4cee
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-07-20 23:21:19 +0530

    Add loader

--- 22RoutingPractice/src/components/PostCard.jsx
+++ 22RoutingPractice/src/components/PostCard.jsx
@@ -6,13 +6,15 @@ function PostCard() {
     let params = useParams()
     console.log(params)
 
-    const [itemData, setItemData] = useState([])
+    const [itemData, setItemData] = useState(null)
 
     useEffect(() => {
         fetch(`https://fakestoreapi.com/products/${params.postId}`)
             .then((response) => response.json())
             .then((data) => setItemData(data))
-    }, [])
+        }, [])
+        if(itemData === null) return <h2>loading....</h2>
+
     return (
         <>
             <button style={{ backgroundColor: "Black", padding: "5px 30px" }}>
@@ -22,7 +24,7 @@ function PostCard() {
             <div style={{ maxWidth: "400px", textAlign: "center", boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px", margin: "50px" }}>
                 <img style={{ width: "200px" }} src={itemData.image} alt="" />
                 <h2>{itemData.title}</h2>
-                <p>{itemData?.description?.substring(0, 200)}</p>
+                <p>{itemData.description.substring(0, 200)}</p>
                 <h3>{itemData.price}$</h3>
 
             </div>
--- 22RoutingPractice/src/components/ProductsList.jsx
+++ 22RoutingPractice/src/components/ProductsList.jsx
@@ -11,18 +11,19 @@ function ProductsList() {
     }, [])
 
     return (
-        <div>
+        <div >
+            <button style={{ backgroundColor: "Black", padding: "5px 30px" }}>
+                <NavLink style={{ color: "white", textDecoration: "none" }} to={'/'}>Back</NavLink>
+            </button>
             {
                 products.map((item) => (
-                    <div style={{padding:'10px'}} key={item.id}>
+                    <div style={{textAlign:"center", padding:'10px', boxShadow:'rgba(0, 0, 0, 0.16) 0px 1px 4px',maxWidth:'500px', margin:'20px 0'}} key={item.id}>
                         <NavLink style={{textDecoration:"none", color:'black', cursor:'pointer'}} to={`/post/${item.id}`}>{item.title}</NavLink>
                     </div>
                 ))
             }
 
-            <button style={{ backgroundColor: "Black", padding: "5px 30px" }}>
-                <NavLink style={{ color: "white", textDecoration: "none" }} to={'/'}>Back</NavLink>
-            </button>
+            
         </div>
     )
 }
