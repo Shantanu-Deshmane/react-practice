4fddfbbb06110da460f09cda4b8293d5fffd3cb2
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-07-20 12:20:57 +0530

    Error Handling

--- 21WeatherApplication/src/components/Card.jsx
+++ 21WeatherApplication/src/components/Card.jsx
@@ -4,13 +4,19 @@ import { useWeather } from "../context/WeatherContext";
 
 function Card() {
     const weather = useWeather();
-    return (
-    <div className='card-data' style={{display:weather.data ? "block" : "none"}}>
-        <img src={weather.data?.current?.condition?.icon} alt="weatherCondition Image" />
-        <h2>{weather.data?.current?.temp_c}°C</h2>
-        <p>{weather.data?.location?.name}, {weather.data?.location.region}, {weather.data?.location.country} </p>
-    </div>
-  )
+    try {
+        return (
+            <div className='card-data' style={{display:weather.data ? "block" : "none"}}>
+                <img src={weather.data?.current?.condition?.icon} alt="weatherCondition Image" />
+                <h2>{weather.data?.current?.temp_c}°C</h2>
+                <p>{weather.data?.location?.name}, {weather.data?.location.region}, {weather.data?.location.country} </p>
+            </div>
+          )
+    } catch (error) {
+        console.log("Error Occured : "+error)
+        return (
+            <p className='err'>{weather.data?.error?.message}</p>
+    )}
 }
 
 export default Card 
