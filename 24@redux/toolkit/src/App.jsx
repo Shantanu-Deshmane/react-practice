a5caab8d826293a839d38f640176347d09615f8b
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-07-23 19:46:48 +0530

    learn redux-toolkit

---
+++ 24@redux/toolkit/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Cart from './components/Cart';
+import Products from './components/Products'
+import { useEffect, useState } from "react";
+
+function App() {
+  const [productData, setProductData] = useState([])
+useEffect(() => {
+  fetch('https://fakestoreapi.com/products')
+  .then((res) => res.json())
+  .then((data) => setProductData(data))
+},[])
+console.log(productData.map((item) => item.price))
+
+  return (
+    <>
+            <Cart />
+
+      <div style={{display:"flex",flexWrap:"wrap", justifyContent:"space-evenly"}}>
+        <Products data = {productData} />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ 24@redux/toolkit/src/components/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Cart() {
+  return (
+    <div>
+        <div >
+            <h2>Totlal items= 5 (Rs. 3000) /-</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Cart
